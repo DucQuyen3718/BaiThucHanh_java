a5bd336871e35238063cdf5b6c0825189dccfa4f
22112002aA@ <phamduc3718@gmail.com>
2023-03-01 17:27:43 +0700

    Bai 28/02

---
+++ BaiThucHanh28_02/src/slide55.java
@@ -0,0 +1,15 @@
+public class slide55 {
+    public static void main(String[] args) {
+        System.out.println("Cac so chia het cho 5 tu 0 - 100 la");
+        for (int i = 0; i <= 100; i++) {
+            if (i % 5 !=0){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
+   
+
+        
+    
